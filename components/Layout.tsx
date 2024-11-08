08c40d167a63c4d2112dbcaa13da21304e3b86ab
Akbar1007 <a.akbarjon007@gmail.com>
2024-11-08 20:11:56 +0500

    clear

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,25 +1,22 @@
-import HeaderContainer from "./headers";
-import Footer from "./footer";
 type Props = {
-  children: any;
-};
+	children: any
+}
 
 export default function Layout({ children }: Props) {
-  return (
-    <div className="wrapper">
-      <HeaderContainer />
-      <main>{children}</main>
-      <Footer />
-      <style jsx>{`
-        .wrapper {
-          display: flex;
-          min-height: 100vh;
-          flex-direction: column;
-        }
-        main {
-          flex: 1;
-        }
-      `}</style>
-    </div>
-  );
+	return (
+		<div className='wrapper'>
+			<main>{children}</main>
+
+			<style jsx>{`
+				.wrapper {
+					display: flex;
+					min-height: 100vh;
+					flex-direction: column;
+				}
+				main {
+					flex: 1;
+				}
+			`}</style>
+		</div>
+	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,3 @@
-import SEO from "./seo";
-import Hero from "../components/hero";
-import Content from "../components/content"
-
 export default function Home() {
-  return (
-    <>
-      <SEO/>
-      <Hero/>
-      <Content/>
-    </>
-  );
+	return <>Hello world</>
 }
