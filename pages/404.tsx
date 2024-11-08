a851b2dfc6ed80d492ac55c3ce2f3ec970903e05
Akbar1007 <a.akbarjon007@gmail.com>
2024-11-08 20:03:57 +0500

    change gitignore

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,5 @@
-import style from "../assets/style/style.module.scss"
-
-
+import style from '../assets/style/style.module.scss'
 
 export default function Custom404() {
-    return <div className={style.container}>404 - Page Not Found :(</div>
-  }
+	return <div className={style.container}>404 - Hello :(</div>
+}
